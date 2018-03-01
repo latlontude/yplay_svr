42c697a995f89018814701500f57b2300355b0c8
franskhi <franskhi@outlook.com>
2018-03-01 22:33:58 +0800

    拉取院系为空时,补充一个默认的

--- src/api/account/do_get_depts_by_school.go
+++ src/api/account/do_get_depts_by_school.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"net/http"
+	"svr/cache"
 	"svr/st"
 )
 
@@ -25,6 +26,15 @@ func doGetDeptsBySchool(req *GetDeptsBySchoolReq, r *http.Request) (rsp *GetDept
 	if err != nil {
 		log.Errorf("uin %d, GetDeptsBySchoolRsp error, %s", req.Uin, err.Error())
 		return
+	}
+
+	//如果发现某个学校院系是空，则补充一个默认的
+	if len(depts) == 0 {
+		if _, ok := cache.SCHOOLS[req.SchoolId]; ok {
+			deptId := req.SchoolId*1000 + 1
+			depts = make([]*st.DeptInfo, 0)
+			depts = append(depts, &st.DeptInfo{deptId, "其他院系"})
+		}
 	}
 
 	rsp = &GetDeptsBySchoolRsp{depts}
