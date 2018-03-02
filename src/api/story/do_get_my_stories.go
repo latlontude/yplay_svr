0228ae4f37c116b974ae3ff0101bd8f7d247edfd
changwenkai <changwenkai@yeejay.com>
2018-03-02 22:32:38 +0800

    添加获取我的新闻总数和获取好友的新闻列表

--- src/api/story/do_get_my_stories.go
+++ src/api/story/do_get_my_stories.go
@@ -263,3 +263,200 @@ func GetMyStories(uin int64, ts int64, cnt int) (stories []*st.StoryInfo, err er
 	log.Debugf("end GetMyStories uin:%d", uin)
 	return
 }
+
+func GetMyStoriesCnt(uin int64) (total int, err error) {
+	log.Debugf("start GetMyStoriesCnt uin:%d", uin)
+
+	if uin <= 0 {
+		err = rest.NewAPIError(constant.E_INVALID_PARAM, "invalid params")
+		log.Errorf(err.Error())
+		return
+	}
+
+	app, err := myredis.GetApp(constant.ENUM_REDIS_APP_MY_STORY_LIST)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+
+	keyStr := fmt.Sprintf("%d", uin)
+	//先删除24小时之前发表的
+	expireTs := time.Now().UnixNano()/1000000 - 86400000
+	_, err = app.ZRemRangeByScore(keyStr, 0, expireTs)
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+
+	cnt, err := app.ZCard(keyStr)
+	if err != nil {
+		log.Errorf(err.Error())
+		log.Debugf("get stories count error")
+		return
+	}
+
+	total = cnt
+	log.Debugf("end GetMyStoriesCnt uin:%d", uin)
+	return
+}
+
+func GetUserStories(uin, uid, ts int64, cnt int) (stories []*st.StoryInfo, err error) {
+
+	log.Debugf("start GetUserStories uin:%d, uid:%d", uin, uid)
+	stories = make([]*st.StoryInfo, 0)
+
+	if uin <= 0 || uid <= 0 || ts <= 0 {
+		err = rest.NewAPIError(constant.E_INVALID_PARAM, "invalid params")
+		log.Errorf(err.Error())
+		return
+	}
+
+	app, err := myredis.GetApp(constant.ENUM_REDIS_APP_MY_STORY_LIST)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+
+	keyStr := fmt.Sprintf("%d", uid)
+
+	//获取24小时之前发表的
+	expireTs := time.Now().UnixNano()/1000000 - 86400000
+	vals, err := app.ZRangeByScoreWithoutLimit(keyStr, -1, expireTs)
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+
+	if len(vals) > 0 { // 有24小时之前发表的动态
+
+		log.Debugf("have %d subjects before 24 hours", len(vals))
+
+		app1, err1 := myredis.GetApp(constant.ENUM_REDIS_APP_STORY_STAT)
+		if err1 != nil {
+			log.Error(err1.Error())
+			return
+		}
+
+		//从动态观看总数列表列表中移除该用户24小时之前发表的动态
+		keyStr1 := fmt.Sprintf("total")
+		_, err1 = app1.ZMRem(keyStr1, vals)
+		if err1 != nil {
+			log.Error(err1.Error())
+			return
+		}
+
+	} else {
+		log.Debugf("no subjects before 24 hours")
+	}
+
+	//先删除24小时之前发表的
+	_, err = app.ZRemRangeByScore(keyStr, 0, expireTs)
+	if err != nil {
+		log.Errorf(err.Error())
+	}
+
+	//获取新的STORY ID
+	valsStr, err := app.ZRevRangeByScoreWithScores(keyStr, ts-1, -1, 0, cnt)
+	if err != nil {
+
+		//如果KEY不存在,feed则为空
+		if e, ok := err.(*rest.APIError); ok {
+			if e.Code == constant.E_REDIS_KEY_NO_EXIST {
+				err = nil
+				return
+			}
+		}
+
+		log.Errorf(err.Error())
+		return
+	}
+
+	//没有最新story
+	if len(valsStr) == 0 {
+		log.Debugf("have not new story")
+		return
+	}
+
+	log.Debugf("valsStr:%+v", valsStr)
+
+	if len(valsStr)%2 != 0 {
+		err = rest.NewAPIError(constant.E_REDIS_ZSET, "ZRevRangeWithScore return values cnt not even(2X)")
+		log.Error(err.Error())
+		return
+	}
+
+	var lastVid int64
+	var lastMs int64
+
+	orderVids := make([]int64, 0)
+
+	for i, valStr := range valsStr {
+
+		if i%2 == 0 {
+			lastVid, err = strconv.ParseInt(valStr, 10, 64)
+			if err != nil {
+				err = rest.NewAPIError(constant.E_REDIS_ZSET, "ZRevRangeWithScore value not interge")
+				log.Error(err.Error())
+				return
+			}
+
+		} else {
+
+			lastMs, err = strconv.ParseInt(valStr, 10, 64)
+			if err != nil {
+				err = rest.NewAPIError(constant.E_REDIS_ZSET, "ZRevRangeWithScore value not interge")
+				log.Error(err.Error())
+				return
+			}
+
+			if lastMs > 0 && lastVid > 0 {
+				orderVids = append(orderVids, lastVid)
+			}
+		}
+	}
+
+	log.Debugf("orderVids:%+v", orderVids)
+
+	if len(orderVids) == 0 {
+		return
+	}
+
+	app2, err := myredis.GetApp(constant.ENUM_REDIS_APP_STORY_MSG)
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+
+	storyIds := make([]string, 0)
+	for _, vid := range orderVids {
+		storyIds = append(storyIds, fmt.Sprintf("%d", vid))
+	}
+
+	storyVals, err := app2.MGet(storyIds)
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+
+	log.Debugf("storyVals:%+v", storyVals)
+
+	for _, svid := range storyIds {
+
+		storyVal, ok := storyVals[svid]
+		if !ok {
+			continue
+		}
+
+		var si st.StoryInfo
+		err = json.Unmarshal([]byte(storyVal), &si)
+		if err != nil {
+			log.Errorf(err.Error())
+			return
+		} else {
+			stories = append(stories, &si)
+
+		}
+	}
+	log.Debugf("end GetUserStories")
+	return
+}
