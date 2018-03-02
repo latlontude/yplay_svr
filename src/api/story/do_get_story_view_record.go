e31579c87274fa46127abab327592f026aa4b376
changwenkai <changwenkai@yeejay.com>
2018-03-02 09:41:30 +0800

    update or get view records

---
+++ src/api/story/do_get_story_view_record.go
@@ -0,0 +1,98 @@
+package story
+
+import (
+	"common/constant"
+	"common/myredis"
+	"common/rest"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type GetStoryViewRecordReq struct {
+	Uin     int64  `schema:"uin"`
+	Token   string `schema:"token"`
+	Ver     int    `schema:"ver"`
+	StoryId int64  `schema:"storyId"`
+}
+
+type GetStoryViewRecordRsp struct {
+	viewInfo map[int64]int64 `json:"viewInfo"`
+}
+
+func doGetStoryViewRecord(req *GetStoryViewRecordReq, r *http.Request) (rsp *GetStoryViewRecordRsp, err error) {
+
+	log.Debugf("uin %d, GetStoryViewRecordReq %+v", req.Uin, req)
+
+	ret, err := GetStoryViewRecord(req.StoryId)
+	if err != nil {
+		log.Errorf("uin %d, GetStoryViewRecordRsp error, %s", req.Uin, err.Error())
+		return
+	}
+
+	rsp = &GetStoryViewRecordRsp{ret}
+
+	log.Debugf("uin %d, GetStoryViewRecordRsp succ, %+v", req.Uin, rsp)
+
+	return
+}
+
+func GetStoryViewRecord(storyId int64) (ret map[int64]int64, err error) {
+	log.Debugf("start GetStoryViewRecord storyId:%d", storyId)
+
+	if storyId <= 0 {
+		err = rest.NewAPIError(constant.E_INVALID_PARAM, "invalid params")
+		log.Errorf(err.Error())
+		return
+	}
+
+	app, err := myredis.GetApp(constant.ENUM_REDIS_APP_STORY_STAT)
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+	keyStr := fmt.Sprintf("%d", storyId)
+	vals, err := app.ZRevRangeWithScores(keyStr, 0, -1)
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+
+	if len(vals) == 0 {
+		log.Debugf("nobody scan this msg")
+		return
+	}
+
+	log.Debugf("valsStr:%+v", vals)
+
+	var viewUid int64
+	var viewTs int64
+
+	for i, val := range vals {
+
+		if i%2 == 0 {
+			viewUid, err = strconv.ParseInt(val, 10, 64)
+			if err != nil {
+				err = rest.NewAPIError(constant.E_REDIS_ZSET, "ZRevRangeWithScore value not interge")
+				log.Error(err.Error())
+				return
+			}
+
+		} else {
+
+			viewTs, err = strconv.ParseInt(val, 10, 64)
+			if err != nil {
+				err = rest.NewAPIError(constant.E_REDIS_ZSET, "ZRevRangeWithScore value not interge")
+				log.Error(err.Error())
+				return
+			}
+
+			if viewTs > 0 && viewUid > 0 {
+				ret[viewUid] = viewTs
+			}
+		}
+	}
+
+	log.Debugf("end GetStoryViewRecord storyId:%d", storyId)
+	return
+}
---
+++ src/api/story/do_update_story_view_record.go
@@ -0,0 +1,101 @@
+package story
+
+import (
+	"common/constant"
+	"common/myredis"
+	"common/rest"
+	"fmt"
+	"net/http"
+	//"strconv"
+)
+
+type UpdateStoryViewRecordReq struct {
+	Uin     int64  `schema:"uin"` // 观看此条动态的用户
+	Token   string `schema:"token"`
+	Ver     int    `schema:"ver"`
+	StoryId int64  `schema:"storyId"` //观看的动态id
+	Ts      int64  `schema:"ts"`      //  观看动态的时间
+}
+
+type UpdateStoryViewRecordRsp struct {
+}
+
+func doUpdateStoryViewRecord(req *UpdateStoryViewRecordReq, r *http.Request) (rsp *UpdateStoryViewRecordRsp, err error) {
+
+	log.Debugf("uin %d, UpdateStoryViewRecordReq %+v", req.Uin, req)
+
+	err = UpdateStoryViewRecord(req.Uin, req.StoryId, req.Ts)
+	if err != nil {
+		log.Errorf("uin %d, UpdateStoryViewRecordRsp error, %s", req.Uin, err.Error())
+		return
+	}
+
+	rsp = &UpdateStoryViewRecordRsp{}
+
+	log.Debugf("uin %d, UpdateStoryViewRecordRsp succ, %+v", req.Uin, rsp)
+
+	return
+}
+
+func UpdateStoryViewRecord(uin, storyId, ts int64) (err error) {
+
+	log.Debugf("start UpdateStoryViewRecord")
+
+	if uin <= 0 || storyId <= 0 || ts <= 0 {
+		err = rest.NewAPIError(constant.E_INVALID_PARAM, "invalid params")
+		log.Errorf(err.Error())
+		return
+	}
+
+	app, err := myredis.GetApp(constant.ENUM_REDIS_APP_STORY_STAT)
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+
+	keyStr := fmt.Sprintf("%d", storyId)
+	member := fmt.Sprintf("%d", uin)
+
+	exist, err := app.Exist(keyStr)
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+
+	if !exist {
+		score := ts // 该动态第一次被观看
+		err = app.ZAdd(keyStr, score, member)
+		if err != nil {
+			log.Errorf(err.Error())
+			return
+		}
+		err1 := app.Pexpire(keyStr, 86400) // 24小时过期
+		if err1 != nil {
+			log.Errorf(err1.Error())
+			return
+		}
+		return
+	}
+
+	score, err := app.ZScore(keyStr, member)
+	if err != nil {
+		log.Errorf("zscore err")
+		log.Errorf(err.Error())
+		return
+	}
+
+	if score != 0 { // 此用户已经观看过此条动态
+		log.Debugf("uin:%d has viewed this subject")
+		return
+	}
+
+	score = ts // 此用户第一次观看此条动态
+	err = app.ZAdd(keyStr, score, member)
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+
+	log.Debugf("end UpdateStoryViewRecord")
+	return
+}
