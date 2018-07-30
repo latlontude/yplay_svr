137a018f903970dcae87a18f71903b39e73d0a0e
lyn <lyn@yeejay.com>
2018-07-30 17:30:58 +0800

    #### 问题列表 execl导入数据库

---
+++ src/api/question/auto_input.go
@@ -0,0 +1,80 @@
+package question
+
+import (
+	"common/constant"
+	"common/mydb"
+	"common/rest"
+	"github.com/Luxurioust/excelize"
+	"net/http"
+	"time"
+)
+
+type AutoInputReq struct {
+	Uin   int64  `schema:"uin"`
+	Token string `schema:"token"`
+	Ver   int    `schema:"ver"`
+}
+
+type AutoInputRsp struct {
+}
+
+//
+func doAutoInput(req *AutoInputReq, r *http.Request) (rsp *AutoInputRsp, err error) {
+	//去除首位空白字符
+	AutoInput()
+
+	return
+
+}
+
+func AutoInput() (uids []int64, err error) {
+
+	inst := mydb.GetInst(constant.ENUM_DB_INST_YPLAY)
+	if inst == nil {
+		err = rest.NewAPIError(constant.E_DB_INST_NIL, "db inst nil")
+		log.Error(err)
+		return
+	}
+
+	//v2question表多加了一个字段  (同问sameAskUid)
+	stmt, err := inst.Prepare(`insert into auto_question(id, boardId, qTitle, qContent, qImgUrls, isAnonymous, qStatus, createTs) 
+		values(?, ?, ?, ?, ?, ?, ?, ?)`)
+
+	if err != nil {
+		err = rest.NewAPIError(constant.E_DB_PREPARE, err.Error())
+		log.Error(err.Error())
+		return
+	}
+	defer stmt.Close()
+
+	ts := time.Now().Unix()
+
+	xlsx, err := excelize.OpenFile("/home/work/yplay_svr/etc/random_question.xlsx")
+	if err != nil {
+		log.Errorf("error:%v", err)
+		return
+	}
+	// Get value from cell by given sheet index and axis.
+	//cell := xlsx.GetCellValue("Sheet1", "B2")
+	//fmt.Println(cell)
+	// Get sheet index.
+	//index := xlsx.GetSheetIndex("Sheet1")
+	// Get all the rows in a sheet.
+	lineInfo := xlsx.GetRows("Sheet1")
+
+	var qTitle, qImgUrls string
+	for _, row := range lineInfo {
+		log.Debugf("boardId:%s,info:%s,status:%s", row[0], row[1], row[2])
+		_, err = stmt.Exec(0, row[0], qTitle, row[1], qImgUrls, row[2], 0, ts)
+		if err != nil {
+			err = rest.NewAPIError(constant.E_DB_EXEC, err.Error())
+			log.Error(err.Error())
+			return
+		}
+
+		log.Debugf("input a line :%+v", row)
+
+		//return
+	}
+	return
+}
---
+++ src/api/question/auto_question.go
@@ -0,0 +1,185 @@
+package question
+
+import (
+	"common/constant"
+	"common/mydb"
+	"common/myredis"
+	"common/rest"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type AutoQuestionReq struct {
+	Uin     int64  `schema:"uin"`
+	Token   string `schema:"token"`
+	Ver     int    `schema:"ver"`
+	BoardId int    `schema:"boardId"`
+}
+
+type AutoQuestionRsp struct {
+	ids      []int   `json:"ids"`
+	Qids     []int64 `json:"qids"`
+	RestTime int64   `json:"restTime"` //剩余时间
+	Msg      string  `json:"msg"`
+}
+
+//
+func doAutoQuestion(req *AutoQuestionReq, r *http.Request) (rsp *AutoQuestionRsp, err error) {
+	//去除首位空白字符
+	ids, qids, restTime, msg, err := AutoQuestion(req.BoardId)
+
+	if err != nil {
+
+	}
+
+	rsp = &AutoQuestionRsp{ids, qids, restTime, msg}
+
+	log.Debugf("AutoQuestionRsp : %+v", rsp)
+
+	return
+
+}
+
+func AutoQuestion(boardId int) (ids []int, qids []int64, restTime int64, msg string, err error) {
+
+	app, err := myredis.GetApp(constant.ENUM_REDIS_APP_STORY_STAT)
+	if err != nil {
+		log.Errorf(err.Error())
+		//return
+	}
+
+	key := fmt.Sprintf("auto_question_%d", boardId)
+	storeTime, err := app.Get(key)
+
+	if err != nil {
+
+	}
+
+	now := time.Now().Unix()
+	tmp, _ := strconv.ParseInt(storeTime, 10, 64)
+	restTime = now - tmp
+
+	if restTime < 300 {
+		msg = "please wait 5 min"
+		return
+	}
+
+	inst := mydb.GetInst(constant.ENUM_DB_INST_YPLAY)
+	if inst == nil {
+		err = rest.NewAPIError(constant.E_DB_INST_NIL, "db inst nil")
+		log.Error(err)
+		return
+	}
+
+	auto_question_cnt := 0
+	sql := fmt.Sprintf(`select id,boardId,qTitle,qContent,qImgUrls,isAnonymous from auto_question where qStatus = 0 and boardId = %d limit 5`, boardId)
+	rows, err := inst.Query(sql)
+	if err != nil {
+		err = rest.NewAPIError(constant.E_DB_QUERY, err.Error())
+		log.Error(err)
+		return
+	}
+
+	defer rows.Close()
+
+	ids = make([]int, 0)
+	qids = make([]int64, 0)
+
+	index := 0
+
+	randomUids, err := GetRandomUids()
+	if err != nil {
+		log.Errorf("query random uids error")
+	}
+
+	randomLength := len(randomUids)
+
+	for rows.Next() {
+		var id int
+		var boardId int
+		var qTitle string
+		var qContent string
+		var qImgUrls string
+		var isAnonymous bool
+		rows.Scan(&id, &boardId, &qTitle, &qContent, &qImgUrls, &isAnonymous)
+		qid, err := PostQuestion(randomUids[index], boardId, qTitle, strings.Trim(qContent, " \n\t"), qImgUrls, isAnonymous)
+		if err != nil {
+
+		}
+		ids = append(ids, id)
+		qids = append(qids, qid)
+
+		index++
+		if index > randomLength {
+			index = 0
+		}
+		auto_question_cnt++
+	}
+
+	//题库没有题了 直接返回
+	if auto_question_cnt == 0 {
+		msg = "rest question is zero"
+		return
+	}
+
+	var idstr string
+	for i, id := range ids {
+		if i != len(ids)-1 {
+			idstr += fmt.Sprintf(`%d,`, id)
+		} else {
+			idstr += fmt.Sprintf(`%d`, id)
+		}
+	}
+
+	log.Debugf("test ids = %s", idstr)
+	sql = fmt.Sprintf(`update auto_question set qStatus = 1  where id in (%s) and boardId = %d `, idstr, boardId)
+	_, err = inst.Query(sql)
+
+	if err != nil {
+		err = rest.NewAPIError(constant.E_DB_QUERY, err.Error())
+		log.Error(err)
+		return
+	}
+
+	value := fmt.Sprintf("%d", now)
+	err = app.Set(key, value)
+
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+
+	msg = "success"
+
+	return
+}
+
+func GetRandomUids() (uids []int64, err error) {
+	inst := mydb.GetInst(constant.ENUM_DB_INST_YPLAY)
+	if inst == nil {
+		err = rest.NewAPIError(constant.E_DB_INST_NIL, "db inst nil")
+		log.Error(err)
+		return
+	}
+	sql := fmt.Sprintf(`select uin from profiles where locate('1406666032',phone) or locate('1406666035',phone)  ORDER BY RAND() limit 5`)
+	rows, err := inst.Query(sql)
+	if err != nil {
+		err = rest.NewAPIError(constant.E_DB_QUERY, err.Error())
+		log.Error(err)
+		return
+	}
+	defer rows.Close()
+
+	uids = make([]int64, 0)
+	for rows.Next() {
+		var uid int64
+		rows.Scan(&uid)
+		uids = append(uids, uid)
+	}
+
+	log.Debugf("uids:%+v", uids)
+	return
+}
